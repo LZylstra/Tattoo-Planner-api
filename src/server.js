d00ad0d57780e2148bf2c4a09c8d4f8c48d8c3f2
LZylstra <lindseyzylstra@gmail.com>
2020-02-15 13:46:42 -0800

    set up cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/api/*", (req, res) => {
   res.json({ ok: true });
