b9b28af1149d2ee7c868fe71e092dc8f97eb65f3
LZylstra <lindseyzylstra@gmail.com>
2020-02-16 22:46:38 -0800

    updated for deployment

--- seeds/seed.tattoo_tables.sql
+++ seeds/seed.tattoo_tables.sql
@@ -4,8 +4,7 @@ TRUNCATE tattoo_users;
 
 INSERT INTO tattoo_users (user_name, full_name, password)
 VALUES
-('lzylstra', 'Lindsey Zylstra', 'password'),
-('azylstra', 'Amanda Zylstra', 'password'),
-('demo', 'Demo User', 'password')
+('lzylstra', 'Lindsey Zylstra', '$2a$12$d1t6lPTxudxoqWG2dCfZ4u9R0lXUE4RFC2dzqHi5mUM/DC7D8VIqm'),
+('demo', 'Demo User', '$2a$12$d1t6lPTxudxoqWG2dCfZ4u9R0lXUE4RFC2dzqHi5mUM/DC7D8VIqm')
 ;
 COMMIT;
