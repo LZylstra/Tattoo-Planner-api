155dff54cef47b1c400221a75e614f80bcb8d331
LZylstra <lindseyzylstra@gmail.com>
2020-02-16 22:30:36 -0800

    preparation for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://lzylstra@localhost/tattoo-app",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://lzylstra@localhost/tattoo-app",
   JWT_SECRET: process.env.JWT_SECRET || "purplecow",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "15m",
   API_BASE_URL:
