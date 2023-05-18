41b0d2be3f767b884cedcbcda8d98805e66d7e47
Lindsey Zylstra <lindseyzylstra@gmail.com>
2023-05-18 11:34:30 -0700

    Changes to Error Handling and versions

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,16 @@
-require("dotenv").config();
 const path = require("path");
-const { DATABASE_URL, DB_USERNAME, DB_PW } = process.env;
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
-    connection: 'DATABASE_URL',
-    user:     'DB_USERNAME',
-    password: 'DB_PW',
+    client: "postgresql",
+    connection: DATABASE_URL,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations")
-    }
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const morgan = require("morgan");
-const errorHandler = require("./middleware/error-handler");
 const cors = require("cors");
+
+const morgan = require("morgan");
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
 const helmet = require("helmet");
 //const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
 const { NODE_ENV } = require("./config");
@@ -15,30 +18,19 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-// Not found handler
-app.use((req, res, next) => {
-    next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
-
-  // Error handler
-app.use((error, req, res, next) => {
-    console.error(error);
-    const { status = 500, message = "Something went wrong!" } = error;
-    res.status(status).json({ error: message });
-});
-
 app.use(cors());
 app.use(morgan(morganOption));
 app.use(helmet());
 
 app.options("*", cors());
 
-app.use("/api/events", eventsRouter, cors());
-app.use("/api/tattoos", tattoosRouter);
-app.use("/api/clients", clientsRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/users", usersRouter);
+//app.use("/api/events", eventsRouter, cors());
+//app.use("/api/tattoos", tattoosRouter);
+//app.use("/api/clients", clientsRouter);
+//app.use("/api/auth", authRouter);
+//app.use("/api/users", usersRouter);
 
+app.use(notFound);
 app.use(errorHandler);
 
 module.exports = app;
